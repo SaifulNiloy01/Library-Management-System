5be63cd39c3b03a2007600846d919513ee7879b0
SaifulNiloy01 <saifulniloy15@gmail.com>
2025-04-20 20:53:21 +0600

    shared module file added in staging (LibraryUI)

--- LibraryUI/src/app/shared/shared.module.ts
+++ LibraryUI/src/app/shared/shared.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material/material.module';
 import { PageHeaderComponent } from './components/page-header/page-header.component';
+import { PageFooterComponent } from './components/page-footer/page-footer.component';
 
 
 
 @NgModule({
-  declarations: [PageHeaderComponent],
+  declarations: [PageHeaderComponent, PageFooterComponent],
   imports: [CommonModule, MaterialModule],
   exports: [CommonModule, MaterialModule, PageHeaderComponent],
 })
