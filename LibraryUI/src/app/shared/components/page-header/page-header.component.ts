ef9ac0f64a11b0574a7f7f847e01949e5d89eb83
SaifulNiloy01 <saifulniloy15@gmail.com>
2025-04-20 17:23:29 +0600

    adding Page Header (Shared component)

---
+++ LibraryUI/src/app/shared/components/page-header/page-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-header',
+  standalone: false,
+  templateUrl: './page-header.component.html',
+  styleUrl: './page-header.component.scss'
+})
+export class PageHeaderComponent {
+
+}
