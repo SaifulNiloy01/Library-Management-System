4beecfca2425e5826ff10cc1f7fbfa3e2d223eb3
SaifulNiloy01 <saifulniloy15@gmail.com>
2025-04-20 20:51:53 +0600

    adding page footer in LibraryUI

---
+++ LibraryUI/src/app/shared/components/page-footer/page-footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-footer',
+  standalone: false,
+  templateUrl: './page-footer.component.html',
+  styleUrl: './page-footer.component.scss'
+})
+export class PageFooterComponent {
+
+}
