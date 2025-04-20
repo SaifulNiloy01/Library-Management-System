7c883dc8fce5f20db778d2f41e6d89914bd144ba
SaifulNiloy01 <saifulniloy15@gmail.com>
2025-04-20 11:14:31 +0600

    Stop tracking .vs folder

--- LibraryAPI/LibraryAPI/obj/Debug/net8.0/LibraryAPI.AssemblyInfo.cs
+++ LibraryAPI/LibraryAPI/obj/Debug/net8.0/LibraryAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b023612ec18e0da2d03568810e947f5c2d60fd79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d22e25a7622b937bf4ddb5f07006a3932f0ed80a")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- LibraryUI/src/app/app.component.ts
+++ LibraryUI/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {MatButtonModule} from '@angular/material/button'
+import {MaterialModule} from './material/material.module'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatButtonModule],
+  imports: [RouterOutlet, MaterialModule, MaterialModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
