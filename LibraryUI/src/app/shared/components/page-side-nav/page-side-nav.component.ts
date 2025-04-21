8dac3d8bd268e2eb2015927c77de2bea57ca4f2d
SaifulNiloy01 <saifulniloy15@gmail.com>
2025-04-21 13:26:46 +0600

    adding page side nav component in LibraryUI

---
+++ LibraryUI/src/app/shared/components/page-side-nav/page-side-nav.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-side-nav',
+  standalone: false,
+  templateUrl: './page-side-nav.component.html',
+  styleUrl: './page-side-nav.component.scss'
+})
+export class PageSideNavComponent {
+
+}
